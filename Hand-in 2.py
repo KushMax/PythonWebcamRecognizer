1d7b99d9d128b47d852a27989e809174c34a84bc
Kush <kushsurfe@gmail.com>
2021-11-24 18:56:11 +0100

    Final

--- Hand-in 2.py
+++ Hand-in 2.py
@@ -1,3 +1,15 @@
+"""
+@Made by: Kush, Hamed, Rasmus & Mansour - 24-11-2021
+Roskilde University - IDS Autumn #2 Hand-in assignment.
+
+Python Webcam Recognizer
+The program is recognizing food labels through the webcam.
+Once you run the program, you'll open a webcam window.
+The program will run until you hit the 'q' button for exit.
+The 'p' button will execute the last seen 'object' and then terminate with
+a print out statement in console, that shows all the ingredients matching with object.
+"""
+
 import requests
 import cv2
 import numpy as np
@@ -7,10 +19,10 @@ from keras import models
 apiKey = "ca87ddf9ee7a96725492d03875534c30"
 appID = "6270b8ac"
 
-model = models.load_model('keras_model.h5')
+model = models.load_model('keras_model.h5', compile=False)
 video = cv2.VideoCapture(0)
 
-ingridients = []
+ingredients = []
 
 while True:
         _, frame = video.read()
@@ -29,7 +41,8 @@ while True:
         'Onion', 'Orange', 'Paprika', 'Pear', 'Pea', 'Pineapple', 'Pomegranate', 'Potato', 'Raddish', 'Soy bean', 'Spinach', 'Sweetpotato', 'Tomato', 'Turnip', 'Watermelon']
 
         print(labels[np.argmax(prediction)])
-        label = labels[np.argmax(prediction)]
+
+        #print(labels[0])
 
         cv2.imshow("Prediction", frame)
 
@@ -38,19 +51,20 @@ while True:
                 break
 
         if key == ord('p'):
-                ingridient = labels[np.argmax(prediction)]+"%20 "
-                ingridients.append(ingridient)
+                ingredient = labels[np.argmax(prediction)]+"%20 "
+                ingredients.append(ingredient)
 
-
+# Exit the video window
 video.release()
 cv2.destroyAllWindows()
-finalingredients = ""
-for x in ingridients:
-        finalingredients+=x
-print(finalingredients)
+
+final_ingredients = ""
+for x in ingredients:
+        final_ingredients+=x
+print(final_ingredients)
 # HTTP request
 url = "https://api.edamam.com/api/recipes/v2?"
-headers = {'type': 'public', 'q': finalingredients, 'app_id': appID, 'app_key': apiKey}
+headers = {'type': 'public', 'q': final_ingredients, 'app_id': appID, 'app_key': apiKey}
 
 r = requests.get(url, params=headers)
 recipes = r.json()
@@ -58,11 +72,11 @@ recipes = r.json()
 # for loop that iterates through the Json document and prints out the first 5 recipes and their ingredients list
 for x in range(0,5):
         try:
-                ingredientLines = recipes['hits'][x]['recipe']['ingredientLines']
+                ingredient_Lines = recipes['hits'][x]['recipe']['ingredientLines']
                 print("recipe ",x+1,":")
                 print(recipes['hits'][x]['recipe']['label'])
                 print("\nIngredients:")
-                for ingredients in ingredientLines:
+                for ingredients in ingredient_Lines:
                         print(ingredients)
                 print("\n")
         except:
